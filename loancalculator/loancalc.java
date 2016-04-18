b1997e32904cf66b08349f131bc3833ba9c9c2db
Yashpal Patel <ypatel@kobo.com>
2016-04-17 22:25:07 -0400

    basic loan calculator

---
+++ loancalculator/loancalc.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class loancalc {
+	public static void main(String[] args) throws Exception {
+		double principal, interestRate, years, emi;
+		Scanner in = new Scanner(System.in);
+
+		System.out.println ("Please enter the principal amount ($): ");
+		principal = in.nextDouble ();
+		System.out.println ("Please enter the interest rate (%): ");
+		interestRate = in.nextDouble ();
+		System.out.println ("Please enter the loan period in years: ");
+		years = in.nextDouble ();
+		
+		double r = interestRate/12/100;
+		emi = Math.round((principal*((r*Math.pow(1+r,years*12))/(Math.pow(1+r,years*12)-1)))*100)/100.;
+		System.out.println ("Your monthly instalment is $"+emi);
+	}
+}
