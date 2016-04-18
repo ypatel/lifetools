83d1cf83ca53fec0cab2addb0a4f981cce77a707
Yashpal Patel <ypatel@kobo.com>
2016-04-17 22:30:00 -0400

    basic loan calculator

--- loancalculator/loancalc.java
+++ loancalculator/loancalc.java
@@ -13,7 +13,7 @@ public class loancalc {
 		years = in.nextDouble ();
 		
 		double r = interestRate/12/100;
-		emi = Math.round((principal*((r*Math.pow(1+r,years*12))/(Math.pow(1+r,years*12)-1)))*100)/100.;
+		emi = Math.round((principal*((r*Math.pow(1+r,years*12))/(Math.pow(1+r,years*12)-1)))*100)/100.00;
 		System.out.println ("Your monthly instalment is $"+emi);
 	}
 }
